9c1044abf28c5c68fb9dbfc6cdea87e468a7bd64
Renato Valencia <renatovalencia@gmail.com>
2018-01-10 09:17:45 -0500

    code documentation and integer validation

--- flatarray.js
+++ flatarray.js
@@ -1,16 +1,23 @@
+/**
+* This function flatens a nested array of integers
+* @method flatArray
+* @param {Array} nestedArray - this parameter is an array of integers that can be nested multiple times. 
+* @return {Array} it will return a flatten array.
+*/
+
 function flatArray (nestedArray) {
 	var newArray = [];
 	nestedArray.forEach((element) => {
-		if (!Array.isArray(element) ) {
+		if (Number.isInteger(element)) {
 			newArray.push(element);
-		} else {
+		} else if (Array.isArray(element)){
 			newArray = newArray.concat(flatArray(element));
 		}
 	});
 	return newArray;
 }
 
-/* Example Usage */
+/* Example Usage, this code is only used for testing purposes*/
 
 var nestedArray = [[0,1],2,3,[4,5,6,[7,8],9],10];
 var flattenarray = flatArray(nestedArray);
