48d7cc5b691e9b567e1f0b9a5999005c68f60651
Renato Valencia <renatovalencia@gmail.com>
2019-12-02 18:53:21 -0500

    optimized use case

--- flatarray.js
+++ flatarray.js
@@ -25,43 +25,23 @@ function flatArray (nestedArray) {
 	return newArray;
 }
 
+
+
 /* Example Usage, this code is only used for testing purposes */
+/* If you would like to test this code using node try: "node flatarray.js" */
 
-try {
-    var nestedArray = [[0,1],2,3,[4,5,6,[7,8],9],10];
-    var flattenarray = flatArray(nestedArray);
-    console.log('flattenarray', flattenarray); 
-}
-catch(error) {
-  console.error(error);
-}
-
-try {
-    var nestedArray2 = [[0,1],2,3,[4,5.86,6,[7,8],9],10];
-    var flattenarray2 = flatArray(nestedArray2);
-    console.log('flattenarray2', flattenarray);
-
-}
-catch(error) {
-  console.error(error);
-}
-
-try {
-    var nestedArray3 = 50;
-    var flattenarray3 = flatArray(flattenarray3);
-    console.log('flattenarray3', flattenarray3);
-
-}
-catch(error) {
-  console.error(error);
-}
-
-try {
-    var nestedArray4 = 'String case';
-    var flattenarray4 = flatArray(flattenarray4);
-    console.log('flattenarray4', flattenarray4);
-
-}
-catch(error) {
-  console.error(error);
-}
+var nestedArray = [[0,1],2,3,[4,5,6,[7,8],9],10],
+    nestedArray2 = [[0,1],2,3,[4,5.86,6,[7,8],9],10],
+    nestedArray3 = 50,
+    nestedArray4 = 'String case',
+    multipleCases = [nestedArray, nestedArray2, nestedArray3, nestedArray4];
+    
+multipleCases.forEach(function(singleCase){
+    try {
+        var flattenarray = flatArray(singleCase);
+        console.log('flattenarray', flattenarray); 
+    }
+    catch(error) {
+      console.error(error);
+    }
+});
